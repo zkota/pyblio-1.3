59c7a5782b1a8f84a1a0b31962cfb876c2767ce2
Frederic Gobry <gobry@pybliographer.org>
2004-02-16 13:12:43 +0100

    fixed bug 889349 (widget selection)
    It is now possible to select which text widget to use.
    
    imported from: gobry@pybliographer.org--public/pybliographer--stable--1.2--patch-69

--- Pyblio/ConfDir/GnomeUI.py
+++ Pyblio/ConfDir/GnomeUI.py
@@ -1,17 +1,55 @@
-import gtk
-
 from Pyblio import Config, Fields
 from Pyblio.GnomeUI import Utils, Editor
+
+import gtk
 
 Config.define ('gnomeui/default', """ Graphical description of the
 default field. """)
 
 Config.define ('gnomeui/monospaced', """ A monospaced font, for native edition """)
 
+def _text_get ():
+
+    v = Config.get ('base/fields').data
+
+    fields = filter (lambda x: x.type is Fields.Text, v.values ())
+    fields = map (lambda x: x.name.lower (), fields)
+
+    fields.sort ()
+    
+    return fields
+
+def _on_multiline_select (item, multi, user):
+
+    h = Config.get ('base/fields').data
+
+    for k, v in multi.items ():
+
+        if not h.has_key (k): continue
+        
+        if v: h [k].widget = Editor.Text
+        else: h [k].widget = Editor.Entry
+        
+    return True
+
+
+Config.define ('gnomeui/multiline',
+               """ Fields displayed in a multi-line widget """,
+
+               Config.Dict (Config.Element (_text_get),
+                            Config.Boolean ()),
+
+               hook = _on_multiline_select)
+
+
 # --------------------------------------------------
+
+
 
 Config.set ('gnomeui/monospaced',
             gtk.load_font ('-*-*-*-r-normal-*-*-*-*-*-c-*-iso8859-1'))
+
+
 
 h = Config.get ('base/fields').data
 
@@ -29,13 +67,16 @@ for f, w in (('author', 150),
              ('booktitle', 200),
              ('date', 50)):
     
-    if not h.has_key (f):
-        continue
+    if not h.has_key (f): continue
 
     h [f].width = w
     
-if h.has_key ('abstract'):
-    h ['abstract'].widget = Editor.Text
 
 Config.set ('gnomeui/default',  (150, gtk.JUSTIFY_LEFT, Editor.Entry))
+
+multi = {}
+
+if h.has_key ('abstract'): multi ['abstract'] = 1
+
+Config.set ('gnomeui/multiline', multi)
 
